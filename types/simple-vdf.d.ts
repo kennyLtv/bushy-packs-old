dc060428a326bdb0f151d2e9e252efa45d392758
Ryan Bucshon <busheezy@gmail.com>
2019-01-23 14:19:55 -0600

    test

---
+++ types/simple-vdf.d.ts
@@ -0,0 +1,5 @@
+declare module 'simple-vdf' {
+  export function dump(obj: any, pretty: any): any;
+  export function parse(text: any): any;
+  export function stringify(obj: any, pretty: any): any;
+}
