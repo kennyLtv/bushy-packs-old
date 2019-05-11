c6c028e72c4f96f3d5b4b3b24ca3926abc955c0f
Ryan Bucshon <busheezy@gmail.com>
2019-05-11 12:48:59 -0500

    push broken tests so I can pull on VM

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,7 +1,4 @@
-function sum(a, b) {
-  return a + b;
-}
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('adds 1 + 2 to equal 3', async () => {
+  // console.log(expect(Promise.resolve()).resolves);
+  // await expect(Promise.resolve()).resolves;
 });
