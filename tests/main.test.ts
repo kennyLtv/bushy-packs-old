9a19f8c5e31b1fb97dd062581311188f28ca7b88
Ryan Bucshon <busheezy@gmail.com>
2019-05-17 15:51:37 -0500

    fix text formatting on tests

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -23,7 +23,7 @@ try {
 test('lazy test for shell output', async (): Promise<void> => {
   const cmd = 'echo foo';
   const prom = execAsync(cmd, {
-    cwd: path.join(__dirname, '..')
+    cwd: path.join(__dirname, '..'),
   });
   await expect(prom).resolves.toEqual('foo');
 });
@@ -31,15 +31,16 @@ test('lazy test for shell output', async (): Promise<void> => {
 test('fails from missing directory arg', async (): Promise<void> => {
   const cmd = 'npm run start -- -c busheezy/bushy-packs-test-config -p test';
   const prom = execAsync(cmd, {
-    cwd: path.join(__dirname, '..')
+    cwd: path.join(__dirname, '..'),
   });
   await expect(prom).rejects.toThrow('You did not specify which directory.');
 });
 
 test('fails from missing preset arg', async (): Promise<void> => {
-  const cmd = 'npm run start -- -c busheezy/bushy-packs-test-config -d ./tests/test-output';
+  const cmd =
+    'npm run start -- -c busheezy/bushy-packs-test-config -d ./tests/test-output';
   const prom = execAsync(cmd, {
-    cwd: path.join(__dirname, '..')
+    cwd: path.join(__dirname, '..'),
   });
   await expect(prom).rejects.toThrow('You did not specify which preset.');
 });
@@ -47,51 +48,67 @@ test('fails from missing preset arg', async (): Promise<void> => {
 test('fails from missing config arg', async (): Promise<void> => {
   const cmd = 'npm run start -- -d ./tests/test-output -p test';
   const prom = execAsync(cmd, {
-    cwd: path.join(__dirname, '..')
+    cwd: path.join(__dirname, '..'),
   });
   await expect(prom).rejects.toThrow('You did not specify a config repo.');
 });
 
 test('run', async (): Promise<void> => {
-  const cmd = 'npm run start -- -c busheezy/bushy-packs-test-config -d ./tests/test-output -p test';
+  const cmd =
+    'npm run start -- -c busheezy/bushy-packs-test-config -d ./tests/test-output -p test';
   const prom = execAsync(cmd, {
-    cwd: path.join(__dirname, '..')
+    cwd: path.join(__dirname, '..'),
   });
 
   await expect(prom).resolves.toBeDefined();
 });
 
 test('shell & scripting runs', async (): Promise<void> => {
-  const cmd = 'npm run start -- -c busheezy/bushy-packs-test-config -d ./tests/test-output -p test -r busheezy/bushy-packs-tests';
+  const cmd =
+    'npm run start -- -c busheezy/bushy-packs-test-config -d ./tests/test-output -p test -r busheezy/bushy-packs-tests';
   const prom = execAsync(cmd, {
-    cwd: path.join(__dirname, '..')
+    cwd: path.join(__dirname, '..'),
   });
-  
+
   await expect(prom).resolves.toContain('The shell has ran.');
   await expect(prom).resolves.toContain('Running: benchmark.sp');
   await expect(prom).resolves.toContain('Running: cstrike-test.sp');
 });
 
 test('normal file is copied', async (): Promise<void> => {
-  await expect(fileAccess(path.join(testOutputDir, 'file.copy.txt'))).resolves.toBeTruthy();
+  await expect(
+    fileAccess(path.join(testOutputDir, 'file.copy.txt')),
+  ).resolves.toBeTruthy();
 });
 
 test('fails on file is not there', async (): Promise<void> => {
-  await expect(fileAccess(path.join(testOutputDir, 'file.not.here.txt'))).resolves.toBeFalsy();
+  await expect(
+    fileAccess(path.join(testOutputDir, 'file.not.here.txt')),
+  ).resolves.toBeFalsy();
 });
 
 test('ensure file was deleted from delete.txt', async (): Promise<void> => {
-  await expect(fileAccess(path.join(testOutputDir, 'file.delete.txt'))).resolves.toBeFalsy();
+  await expect(
+    fileAccess(path.join(testOutputDir, 'file.delete.txt')),
+  ).resolves.toBeFalsy();
 });
 
 test('folder is copied', async (): Promise<void> => {
-  await expect(fileAccess(path.join(testOutputDir, 'folder.copy'))).resolves.toBeTruthy();
-  await expect(fileAccess(path.join(testOutputDir, 'folder.copy', 'test1.txt'))).resolves.toBeTruthy();
-  await expect(fileAccess(path.join(testOutputDir, 'folder.copy', 'test2.txt'))).resolves.toBeTruthy();
+  await expect(
+    fileAccess(path.join(testOutputDir, 'folder.copy')),
+  ).resolves.toBeTruthy();
+  await expect(
+    fileAccess(path.join(testOutputDir, 'folder.copy', 'test1.txt')),
+  ).resolves.toBeTruthy();
+  await expect(
+    fileAccess(path.join(testOutputDir, 'folder.copy', 'test2.txt')),
+  ).resolves.toBeTruthy();
 });
 
 test('vdf merged', async (): Promise<void> => {
-  const outputBuffer = await fs.readFile(path.join(testOutputDir, 'databases.cfg'));
+  const outputBuffer = await fs.readFile(
+    path.join(testOutputDir, 'databases.cfg'),
+  );
   const outputString = outputBuffer.toString();
   const outputObj = vdf.parse(outputString);
 
@@ -112,35 +129,51 @@ test('vdf merged', async (): Promise<void> => {
         user: '4',
         pass: '5',
         port: '6',
-      }
-    }
-  })
+      },
+    },
+  });
 });
 
 test('handlebars var', async (): Promise<void> => {
-  const outputBuffer = await fs.readFile(path.join(testOutputDir, 'file.copy.hb', 'test1.txt'));
-  const compareBuffer = await fs.readFile(path.join(compareToDir, 'file.copy.hb', 'test1.txt'));
+  const outputBuffer = await fs.readFile(
+    path.join(testOutputDir, 'file.copy.hb', 'test1.txt'),
+  );
+  const compareBuffer = await fs.readFile(
+    path.join(compareToDir, 'file.copy.hb', 'test1.txt'),
+  );
 
   expect(outputBuffer).toEqual(compareBuffer);
 });
 
 test('handlebars merged var', async (): Promise<void> => {
-  const outputBuffer = await fs.readFile(path.join(testOutputDir, 'file.copy.hb', 'test2.txt'));
-  const compareBuffer = await fs.readFile(path.join(compareToDir, 'file.copy.hb', 'test2.txt'));
+  const outputBuffer = await fs.readFile(
+    path.join(testOutputDir, 'file.copy.hb', 'test2.txt'),
+  );
+  const compareBuffer = await fs.readFile(
+    path.join(compareToDir, 'file.copy.hb', 'test2.txt'),
+  );
 
   expect(outputBuffer).toEqual(compareBuffer);
 });
 
 test('handlebars incrementer', async (): Promise<void> => {
-  const outputBuffer = await fs.readFile(path.join(testOutputDir, 'file.copy.hb', 'test3.txt'));
-  const compareBuffer = await fs.readFile(path.join(compareToDir, 'file.copy.hb', 'test3.txt'));
+  const outputBuffer = await fs.readFile(
+    path.join(testOutputDir, 'file.copy.hb', 'test3.txt'),
+  );
+  const compareBuffer = await fs.readFile(
+    path.join(compareToDir, 'file.copy.hb', 'test3.txt'),
+  );
 
   expect(outputBuffer).toEqual(compareBuffer);
 });
 
 test('handlebars condition', async (): Promise<void> => {
-  const outputBuffer = await fs.readFile(path.join(testOutputDir, 'file.copy.hb', 'test4.txt'));
-  const compareBuffer = await fs.readFile(path.join(compareToDir, 'file.copy.hb', 'test4.txt'));
+  const outputBuffer = await fs.readFile(
+    path.join(testOutputDir, 'file.copy.hb', 'test4.txt'),
+  );
+  const compareBuffer = await fs.readFile(
+    path.join(compareToDir, 'file.copy.hb', 'test4.txt'),
+  );
 
   expect(outputBuffer).toEqual(compareBuffer);
 });
