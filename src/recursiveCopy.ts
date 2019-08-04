7c1a41b169e7069927043959e05645cf79716580
Ryan Bucshon <busheezy@gmail.com>
2019-08-04 14:20:46 -0500

    add is equal helper

--- src/recursiveCopy.ts
+++ src/recursiveCopy.ts
@@ -23,6 +23,13 @@ handlebars.registerHelper(
   },
 );
 
+Handlebars.registerHelper(
+  'ifEquals',
+  (arg1, arg2, options): boolean => {
+    return arg1 == arg2 ? options.fn(this) : options.inverse(this);
+  },
+);
+
 function handlebarsTransform(envs: EnvVars): Transform {
   return through2(
     {
